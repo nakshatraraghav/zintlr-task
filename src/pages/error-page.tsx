9c33f59bc3ab7c66e69df336c6fae668d582cd38
nakshatra <combatrickshaw@gmail.com>
2023-11-08 13:01:19 +0530

    (routing): setup routing

---
+++ src/pages/error-page.tsx
@@ -0,0 +1,8 @@
+export default function ErrorPage() {
+  return (
+    <div className="h-screen w-full flex flex-col items-center justify-center">
+      <h1 className="text-2xl font-bold">404</h1>
+      <p>This page was not found</p>
+    </div>
+  );
+}
---
+++ src/router/router.tsx
@@ -0,0 +1,53 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Dashboard from "@/pages/dashboard";
+import BullionOperations from "@/pages/bullion-operations";
+import Transactions from "@/pages/transactions";
+import YourNetwork from "@/pages/your-network";
+import Consumers from "@/pages/consumer";
+import OtherOperations from "@/pages/other-operations";
+import ManageBankAccounts from "@/pages/manage-bank-accounts";
+import Faq from "@/pages/faq";
+import CustomizeDashboard from "@/pages/customize-dashboard";
+
+import ErrorPage from "@/pages/error-page";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/bullion-operations",
+    element: <BullionOperations />,
+  },
+  {
+    path: "/transactions",
+    element: <Transactions />,
+  },
+  {
+    path: "/network",
+    element: <YourNetwork />,
+  },
+  {
+    path: "/consumers",
+    element: <Consumers />,
+  },
+  {
+    path: "/other-operations",
+    element: <OtherOperations />,
+  },
+  {
+    path: "/manage-bank-accounts",
+    element: <ManageBankAccounts />,
+  },
+  {
+    path: "/faq",
+    element: <Faq />,
+  },
+  {
+    path: "/customize-dashboard",
+    element: <CustomizeDashboard />,
+  },
+]);
